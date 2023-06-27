cc1e2a1e6c383a8332ddec5c24b58e7e5fc6e552
Misha Rana <misha.rana16@gmail.com>
2023-06-28 01:33:33 +0530

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # eval $(cat ./.env | sed 's/ *= */=/' | sed 's/^/export /')
-export OPENAI_API_KEY=""
+export OPENAI_API_KEY=""   ## Add key here 
 
 uvicorn main:app --host 0.0.0.0 --port 8000 
